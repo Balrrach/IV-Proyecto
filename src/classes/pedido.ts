c2680657b75e854a96029860e678125a3556a3b8
Asmilex <andresmm@outlook.com>
2021-10-16 12:02:24 +0200

    Generación de IDs para Producto y Pedido
    closes Balrrach#15

--- src/classes/pedido.ts
+++ src/classes/pedido.ts
@@ -2,12 +2,19 @@ import { Restaurante, Localizacion } from './localizacion';
 import { Producto } from './producto';
 
 class Pedido {
-  ID = 0;
+  private static last_ID_generated: number = 1
+
+  ID: number = 0;
 
   destino: Localizacion = new Localizacion();
   origen: Restaurante = new Restaurante();
 
   productos_asociados: Producto[] = [];
+
+  private generate_new_ID(): number {
+    Pedido.last_ID_generated++
+    return Pedido.last_ID_generated
+  }
 
   constructor(origen: Restaurante, destino: Localizacion) {}
 }
--- src/classes/producto.ts
+++ src/classes/producto.ts
@@ -1,7 +1,14 @@
 class Producto {
-  ID = -1;
-  nombre = '';
-  peso = 0;
+  private static last_ID_generated: number = 1
+
+  ID: number = 0;
+  nombre: String = '';
+  peso: number = 0;
+
+  private generate_new_ID(): number {
+    Producto.last_ID_generated++
+    return Producto.last_ID_generated
+  }
 
   constructor(nombre: string, peso: number) {}
 }
