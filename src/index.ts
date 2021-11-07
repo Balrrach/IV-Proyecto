5d6c4878a684ed8c35dcc77245aaabd2209dbf16
Baltasar del Sol de Haro <baltasardelsol@gmail.com>
2021-11-07 18:51:50 +0100

    Fix tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+import * as fs from 'fs';
 import{ Gestor } from './classes/gestor';
 import{ Repartidor } from './classes/repartidor';
 
 console.log('Hello world!');
+
+// Read locations
+function readLocations() {
+	fs.readFileSync('data/locations.txt','utf8');
+}
+
+readLocations()
 var gestor = new Gestor
 var repartidor = new Repartidor('Jimmi')
