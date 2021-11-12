11d6f04c80981a35d7a91c4343da2473ccab273c
Baltasar del Sol de Haro <baltasardelsol@gmail.com>
2021-11-12 21:56:13 +0100

    Feat #13
    An order has to be assigned to one and only one delivery_man at any
    given moment, thus it makes sense to have them stored in their
    correspondant delivery_man.
    
    Everytime a route is changed the delivery_man needs to be updated.

--- src/classes/delivery_man.ts
+++ src/classes/delivery_man.ts
@@ -9,19 +9,23 @@ class DeliveryMan {
   private name: string = '';
   private maximumWeight: number = 100;
   private route: Route = new Route()
+  private orders: Order[] = []
 
   private generate_new_ID(): number {
     DeliveryMan.last_ID_generated++;
     return DeliveryMan.last_ID_generated;
   }
 
-
   constructor(name: string, maximumWeight: number = 15000) {}
   
+
+  addOrder(order: Order) {}
+  removeOrder(order: Order) {}
   updateRoute(route: Route) {
 	  this.route = route;
-	  // notifyDeliveryMan();
+	  this.notify();
   }
+  notify() {}
 }
 
 
