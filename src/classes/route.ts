f1759a52d5ebda7efddd4f759c5971fbd78d4ee1
Baltasar del Sol de Haro <baltasardelsol@gmail.com>
2021-11-12 22:18:51 +0100

    Feat #13
    A route is going to have all delivery man orders assign since it is a
    one to one relationship and it has many benefits since calling update it
    can be known if all orders related to the next location are done(since
    orders have states).

--- src/classes/route.ts
+++ src/classes/route.ts
@@ -1,10 +1,13 @@
 import { Restaurant, Location } from './location';
+import { Order } from './order'
+
 
 class Route {
   private destinations: Location[] = [];
-
+  private orders: Order[] = [];
 
   constructor() {}
+
 
   getDestinations(): Location[] {
 	  return this.destinations;
@@ -12,10 +15,10 @@ class Route {
 
 
   next(): Location {
-    return new Location();
+	return this.destinations[0];
   }
 
-  recalculate() {}
+  update() {}
 }
 
 
