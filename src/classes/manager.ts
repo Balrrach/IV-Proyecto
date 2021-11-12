c74a9cdde127f2f6e3b2ca8c1c109cf2003440da
Baltasar del Sol de Haro <baltasardelsol@gmail.com>
2021-11-12 21:19:57 +0100

    Feat #13, #18
    Everything related to the routes is done in the route_calculator, thus
    there is no need for manager to contain list of routes.
    
    If deleting an inexistant restaurant is attempted an exception is
    raised.
    
    When a new order is recieved the assigned delivery man's route needs to
    be updated.

--- src/classes/manager.ts
+++ src/classes/manager.ts
@@ -1,19 +1,56 @@
-import { Map } from './map';
+import { RouteCalculator } from './route_calculator';
+import { Restaurant } from './location';
+import { Order } from './order';
 import { Route } from './route';
+import { DeliveryMan } from './delivery_man';
 
 
 class Manager {
-  private map: Map = new Map();
-  private route: Route = new Route();
+  private routeCalculator: RouteCalculator = new RouteCalculator();
+  private restaurants: Restaurant[] = []
 
-  constructor() {}
 
-  getMap(): Map {
-	  return this.map;
+  constructor(restaurants: Restaurant[] = []) {
+	  this.restaurants = restaurants;
   }
 
-  getRoute(): Route {
-	  return this.route;
+
+  addRestaurant(restaurant: Restaurant) {
+	  this.restaurants.push(restaurant);
+  }
+
+  removeRestaurant(restaurant: Restaurant) {
+	  let index = this.restaurants.indexOf(restaurant);
+	  if( index >= 0) {
+		  this.restaurants.splice(index, 1);
+	  }
+	  else
+		  throw new Error('Tried to delete inexistant restaurant');
+  }
+
+  listRestaurants() {
+	  console.log(this.restaurants);
+  }
+
+
+  addDeliveryMan(deliveryMan: DeliveryMan) {
+	  this.routeCalculator.addDeliveryMan(deliveryMan);
+  }
+
+  removeDeliveryMan(deliveryMan: DeliveryMan) {
+	  this.routeCalculator.removeDeliveryMan(deliveryMan);
+  }
+
+  deliveryManPicksUpProduct(productID: number) {}
+
+
+  addOrder(order: Order) {
+	  let data: [DeliveryMan, Route] = this.routeCalculator.addOrder(order);
+	  this.updateDeliveryManRoute(data);
+  }
+
+  updateDeliveryManRoute(data: [DeliveryMan, Route]) {
+	  data[0].updateRoute(data[1]);
   }
 }
 
