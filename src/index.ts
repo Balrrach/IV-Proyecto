4e0f51de6bbdad0a5f5a36a3244c59a84844a61c
Balrrach <baltasardelsol@gmail.com>
2021-12-27 23:59:52 +0100

    Clean
    No es neceasario esperar a que el objeto de configuración esté preparado
    en index puesto que no se hace un uso explicito del controlador.

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,20 @@ import { Restaurant } from './classes/restaurant';
 import { Product } from './classes/product'
 import { controller } from './controller'
 
+
 // Read locations
-controller.Ready.then( () => {
-	const restaurantsFile = './data/restaurants.json';
-	let restaurants: Promise<Restaurant[]> = loadRestaurants(restaurantsFile);
+const restaurantsFile = './data/restaurants.json';
+let restaurants: Promise<Restaurant[]> = loadRestaurants(restaurantsFile);
 
-	// Instanciate agents
-	restaurants.then(restaurants =>{
-		let manager: Manager = new Manager(restaurants);
-		let deliveryMan: DeliveryMan = new DeliveryMan('Jimmi', [0, 0, 0]);
-		manager.addDeliveryMan(deliveryMan);
-		manager.listRestaurants();
-	})
-
-	let manager: Manager = new Manager();
-	let newRestaurant = new Restaurant([1, 1, 1], "Alfredo's", [new Product('Pollo', 1, 1, 20)])
-	manager.removeRestaurant(newRestaurant)
+// Instanciate agents
+restaurants.then(restaurants =>{
+	let manager: Manager = new Manager(restaurants);
+	let deliveryMan: DeliveryMan = new DeliveryMan('Jimmi', [0, 0, 0]);
+	manager.addDeliveryMan(deliveryMan);
+	manager.listRestaurants();
 })
+
+let manager: Manager = new Manager();
+let newRestaurant = new Restaurant([1, 1, 1], "Alfredo's", [new Product('Pollo', 1, 1, 20)])
+manager.removeRestaurant(newRestaurant)
+
