b7edb86ff1952b114fb5d1a2e7c9a85c0a57256a
Baltasar del Sol de Haro <baltasardelsol@gmail.com>
2021-12-31 15:02:49 +0100

    Feat #36
    La implementación de la API correctamente documentada en el archivo del
    proyecto.

---
+++ src/API/delivery_man.api.ts
@@ -0,0 +1,28 @@
+import { Controller } from "../controller";
+import Hapi from '@hapi/hapi';
+import { pino } from 'pino';
+import * as deliveryMan from '../handlers/delivery_man.handler'
+
+
+const logger: pino.Logger = Controller.getInstance().getLogger();
+const server: Hapi.Server = Controller.getInstance().getServer();
+
+
+server.route({
+	method: 'PUT',
+	path: '/deliveryMan',
+	handler: function(request, h) { deliveryMan.createDeliveryMan(request, h) }
+});
+
+server.route({
+	method: 'POST',
+	path: '/deliveryMan',
+	handler: function(request, h) { deliveryMan.updateDeliveryMan(request, h) }
+});
+
+server.route({
+	method: 'DELETE',
+	path: '/deliveryMan/{id}',
+	handler: function(request, h) { deliveryMan.deleteDeliveryManByID(request, h) }
+});
+
---
+++ src/handlers/product.handler.ts
@@ -0,0 +1,13 @@
+import Hapi from '@hapi/hapi';
+
+
+export function getAllProducts(request: Hapi.Request, h: Hapi.ResponseToolkit) {}
+
+export function getProductByID(request: Hapi.Request, h: Hapi.ResponseToolkit) {}
+
+export function createProduct(request: Hapi.Request, h: Hapi.ResponseToolkit) {}
+
+export function updateProduct(request: Hapi.Request, h: Hapi.ResponseToolkit) {}
+
+export function deleteProductByID(request: Hapi.Request, h: Hapi.ResponseToolkit) {}
+
