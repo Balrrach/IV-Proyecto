20b6eae9aef22ba827d6f2e0bf1299e80c324163
Baltasar del Sol de Haro <baltasardelsol@gmail.com>
2021-12-31 17:28:09 +0100

    Feat #30, #33
    Es conveniente añadir un archivo de configuracion de prueba al proyecto
    para correr los test de la clase de configuración y para que los desarrolladores
    tengan una guía para la creación de su propio archivo de configuracón.

--- test/config.test.ts
+++ test/config.test.ts
@@ -5,24 +5,20 @@ import { Config } from '../src/config'
 it('Server config files are are used when present', () => {})
 
 it('Environment config files are used when present and server config files not present', () => {
-	require('dotenv').config({ path:'./config/configuration.env'});
+	require('dotenv').config({ path:'./config/configuration_test.env'});
 	let noServerConfig = Config.getInstance();
 	let environmentLogDir = process.env.LOG_DIR;
 	let environmentLogFile = process.env.LOG_FILE;
 
-	noServerConfig.ready.then(() => {
-		expect(noServerConfig.getLogDir()).to.equal(environmentLogDir);
-		expect(noServerConfig.getLogFile()).to.equal(environmentLogFile);
-	})
+	expect(noServerConfig.getLogDir()).to.equal(environmentLogDir);
+	expect(noServerConfig.getLogFile()).to.equal(environmentLogFile);
 })
 
 it('Default config files are used when server and evironment config files are not present', () => {
-	require('dotenv').config({ path:'nonexistentPath'});
+	require('dotenv').config({ path:'nonexistent_enviroment_file'});
 	let noServerNoEnvironmentConfig = Config.getInstance();
 
-	noServerNoEnvironmentConfig.ready.then(() => {
-		expect(noServerNoEnvironmentConfig.getLogDir()).to.equal('/tmp/logs/');
-		expect(noServerNoEnvironmentConfig.getLogFile()).to.equal('logs.json');
-	})
+	expect(noServerNoEnvironmentConfig.getLogDir()).to.equal('../tmp/logs/');
+	expect(noServerNoEnvironmentConfig.getLogFile()).to.equal('logs.json');
 })
 
